31df065f1bc9c9322585a529e99dc95a4f654b51
lyl617 <lyl617@mail.ustc.edu.cn>
2018-10-20 21:13:37 +0800

    mysql_web

---
+++ setup.sh
@@ -0,0 +1,10 @@
+systemctl disable firewalld
+systemctl stop firewalld
+
+systemctl start etcd
+systemctl start docker
+systemctl start kube-apiserver
+systemctl start kube-controller-manager
+systemctl start kube-scheduler
+systemctl start kubelet
+systemctl start kube-proxy
